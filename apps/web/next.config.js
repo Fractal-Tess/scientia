866fe42187a89ece4e000a0cb6e144abed474fb0
Fractal-Tess <vgfractal@gmail.com>
2024-02-01 18:46:51 +0200

    pwa

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,10 +3,27 @@
  * for Docker builds.
  */
 await import('./src/env.js');
+import nextPwa from 'next-pwa';
 
 /** @type {import("next").NextConfig} */
-const config = {
+const nextConfig = {
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development' // Remove console.log in production
+  },
   transpilePackages: ['@repo/ui']
 };
+
+// Configuration object tells the next-pwa plugin
+const withPWA = nextPwa({
+  dest: 'public', // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
+const config = withPWA(nextConfig);
 
 export default config;
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,16 +4,29 @@ import { Inter } from 'next/font/google';
 
 import { TRPCReactProvider } from '$/trpc/react';
 import { PropsWithChildren } from 'react';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-sans'
 });
 
-export const metadata = {
-  title: 'Home',
-  description: 'Boilerplate app',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }]
+export const metadata: Metadata = {
+  generator: 'Next.js',
+  manifest: '/manifest.json',
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' }
+  ],
+  authors: [
+    { name: 'Fractal-Tess', url: 'https://www.github.con/Fractal-Tess' }
+  ],
+  viewport:
+    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
+  icons: [
+    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
+    { rel: 'icon', url: 'icons/icon-128x128.png' }
+  ]
 };
 
 export default function RootLayout(props: PropsWithChildren) {
