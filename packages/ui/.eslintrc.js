85a330982167b493729768eea7f30fec75a92486
Fractal-Tess <vgfractal@gmail.com>
2024-02-01 12:06:22 +0200

    added ui package

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -4,6 +4,6 @@ module.exports = {
   extends: ['@repo/eslint-config/react-internal.js'],
   parser: '@typescript-eslint/parser',
   rules: {
-    'no-redeclare': 'off',
-  },
-};
+    'no-redeclare': 'off'
+  }
+}
